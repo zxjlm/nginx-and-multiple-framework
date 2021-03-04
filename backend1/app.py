a48a5c92b01399cd7e9e1de862028c5a46a97cec
SwordHeart <zxjlm233@163.com>
2021-03-04 16:43:51 +0800

    init

--- backend1/app.py
+++ backend1/app.py
@@ -3,20 +3,32 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/api1/bac1')
 def bac1():
-    return {'msg':'hello from flask'}
+    return {'msg': 'hello from flask'}
+
 
 @app.route('/api1/get_bac2/<kk>')
 def get_bac2(kk):
-    res = requests.get('http://api2/api2/item/'+kk)
-    return {'msg':'get data from fastapi','result':res.json()}
+    """
+    调用fastapi的接口
+    :param kk: 一个int参数，参与输出
+    :return:
+    """
+    res = requests.get('http://api2/api2/item/' + kk)
+    return {'msg': 'get data from fastapi', 'result': res.json()}
 
 
 @app.route('/api1/get_bac3/')
 def get_bac3():
-    res = requests.get('http://api3:80/api3/bac3',allow_redirects=False)
-    return {'msg':'get data from node','result':res.json()}
+    """
+    调用node的接口
+    :return:
+    """
+    res = requests.get('http://api3:80/api3/bac3', allow_redirects=False)
+    return {'msg': 'get data from node', 'result': res.json()}
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='0.0.0.0', port=80)
--- backend3/app.js
+++ backend3/app.js
@@ -1,23 +1,49 @@
 const http = require('http');
+const axios = require('axios');
 
 const hostname = '0.0.0.0';
 const port = 80;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  var url = req.url;
-  if (url === '/api3') {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('hellow from node backend');
-  }else if (url === '/api3/bac3') {
-    res.setHeader('Content-Type', 'application/json');
-    res.end('{"data":"node bac, num is 3"}');
-  } else {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('unknown, maybe index?');
-  }
+    res.statusCode = 200;
+    let url = req.url;
+    if (url === '/api3') {
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('hellow from node backend');
+    } else if (url === '/api3/bac3') {
+        res.setHeader('Content-Type', 'application/json');
+        res.end('{"data":"node bac, num is 3"}');
+    } else if (url === '/api3/get_bac2') {
+        axios.get('http://api2/api2/items/1024')
+            .then(response => {
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(response.data));
+            })
+            .catch(error => {
+                console.log(error);
+                res.end('error')
+            });
+    } else if (url === '/api3/get_bac1') {
+        axios.get('http://api1/api1/bac1')
+            .then(response => {
+                console.log(response.data);
+                // console.log(response.data.url);
+                // console.log(response.data.explanation);
+
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(response.data));
+            })
+            .catch(error => {
+                console.log(error);
+
+                res.end('error')
+            });
+    } else {
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('unknown, maybe index?');
+    }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
